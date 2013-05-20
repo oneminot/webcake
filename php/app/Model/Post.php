50df77c49c6bbdc0d99aa440c516fcee421332ea
weborion <hitesh@weborion.in>
2013-05-20 17:50:59 +0530

    My MOngoDB change8

---
+++ php/app/Model/Post.php
@@ -0,0 +1,13 @@
+<?php
+App::uses('AppModel', 'Model');
+class Post extends AppModel {
+ 
+  public $validate = array(
+    'title' => array(
+        'rule' => 'notEmpty'
+    ),
+    'body' => array(
+        'rule' => 'notEmpty'
+    )
+  ); 
+}
